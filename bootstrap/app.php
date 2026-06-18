a791348df3e4e1e5ac27da928cdafe12920e4c35
Dave <davejohncabarrubias@gmail.com>
2026-06-18 22:50:08 +0800

    Add trusted proxy support for Render HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
